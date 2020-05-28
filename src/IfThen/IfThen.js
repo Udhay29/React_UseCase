04de5c4c4274a5d763e7b132a101502fc66b9b6b
Udhay29 <udhayakumar.290793@gmail.com>
2020-05-28 12:51:01 +0530

    usecase1

--- src/IfThen/IfThen.js
+++ src/IfThen/IfThen.js
@@ -5,6 +5,8 @@ import Backend from 'react-dnd-html5-backend'
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import Button from '@atlaskit/button';
 
 function IfThen() {
     const [activeTab, SetActiveTab] = useState(1);
@@ -15,8 +17,28 @@ function IfThen() {
         // the Tabs component knows about the change and re-renders.
         SetActiveTab(selectedTab);
       }
+
+      
+    const fetch = () => {
+        axios.get('https://baskarcompany.atlassian.net/rest/api/3/applicationrole', { 'headers': {  
+            'Authorization': `Basic ${Buffer.from(
+                'baskarvp17@gmail.com:oWM8DIomeuOmKy8b0Sib0214'
+              ).toString('base64')}`,
+              'Access-Control-Allow-Origin': '*',
+              'Accept': 'application/json'
+        } })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            alert(error);
+        })
+
+        
+    }  
     
     return (
+        <div>
         <div className="wrapper">
             <div className="heading">
                 <h4>If node Space Test</h4>
@@ -34,6 +56,10 @@ function IfThen() {
                 </Tab>
                 <Tab eventKey={2} title="Description"><div>Tab 2 content</div></Tab>
             </Tabs>
+        </div>
+        
+        {/* <button onClick={fetch}>button</button> */}
+        <Button onClick={fetch}>My button text</Button>
         </div>
     )
 }
